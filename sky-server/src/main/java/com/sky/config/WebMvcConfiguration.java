683969bbad668a3c1d4929ca076f22ad18747f8f
daykalif <1038200595@qq.com>
2025-02-06 20:31:14 +0800

    feat:Day01-13-Swagger_介绍和使用方式

--- sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
+++ sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
@@ -43,6 +43,7 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
      */
     @Bean
     public Docket docket() {
+        log.info("开始生成接口文档...");
         ApiInfo apiInfo = new ApiInfoBuilder()
                 .title("苍穹外卖项目接口文档")
                 .version("2.0")
@@ -51,7 +52,7 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
         Docket docket = new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.sky.controller"))
+                .apis(RequestHandlerSelectors.basePackage("com.sky.controller"))    // 注意这里，不要忘记写包名
                 .paths(PathSelectors.any())
                 .build();
         return docket;
@@ -60,8 +61,12 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
     /**
      * 设置静态资源映射
      * @param registry
+     *
+     * 如果没有静态资源映射，访问接口会报404；
+     * 访问http://localhost:8080/doc.html会被当成请求api处理了，报404，所以需要设置静态资源映射
      */
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        log.info("开始设置静态资源映射...");
         registry.addResourceHandler("/doc.html").addResourceLocations("classpath:/META-INF/resources/");
         registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
