81f9a9551e54c7bb35f5be481636d772230b3821
daykalif <1038200595@qq.com>
2025-02-07 15:55:56 +0800

    feat:Day02-04-新增员工_代码完善-处理用户名重复异常

--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -1,10 +1,14 @@
 package com.sky.handler;
 
+import com.sky.constant.MessageConstant;
 import com.sky.exception.BaseException;
 import com.sky.result.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+import java.sql.SQLNonTransientException;
 
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
@@ -13,15 +17,34 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @Slf4j
 public class GlobalExceptionHandler {
 
-    /**
-     * 捕获业务异常
-     * @param ex
-     * @return
-     */
-    @ExceptionHandler
-    public Result exceptionHandler(BaseException ex){
-        log.error("异常信息：{}", ex.getMessage());
-        return Result.error(ex.getMessage());
-    }
+	/**
+	 * 捕获业务异常
+	 *
+	 * @param ex
+	 * @return
+	 */
+	@ExceptionHandler
+	public Result exceptionHandler(BaseException ex) {
+		log.error("异常信息：{}", ex.getMessage());
+		return Result.error(ex.getMessage());
+	}
 
+
+	/**
+	 * 处理SQLIntegrityConstraintViolationException异常
+	 *
+	 * @param ex
+	 * @return
+	 */
+	@ExceptionHandler    // @ExceptionHandler用于处理异常，参数为要处理的异常类型；
+	public Result exceptionHandler(SQLIntegrityConstraintViolationException ex) {
+		// Duplicate entry 'zhangsan' for key 'employee.idx_username'
+		String message = ex.getMessage();
+		if (message.contains("Duplicate entry")) {
+			String[] split = message.split(" ");
+			String username = split[2].substring(1, split[2].lastIndexOf("'"));
+			return Result.error(username + MessageConstant.ALREADY_EXISTS);
+		}
+		return Result.error(MessageConstant.UNKNOWN_ERROR);
+	}
 }
