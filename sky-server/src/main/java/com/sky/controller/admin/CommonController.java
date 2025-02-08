809bc7c0dce6c6d5472468d7cd8aeecd230029d3
daykalif <1038200595@qq.com>
2025-02-08 13:55:00 +0800

    feat:Day03-07-新增菜品-代码开发_2-阿里云OSS文件上传

--- sky-server/src/main/java/com/sky/controller/admin/CommonController.java
+++ sky-server/src/main/java/com/sky/controller/admin/CommonController.java
@@ -1,13 +1,19 @@
 package com.sky.controller.admin;
 
+import com.sky.constant.MessageConstant;
 import com.sky.result.Result;
+import com.sky.utils.AliOssUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.UUID;
 
 /*
  * 通用接口
@@ -21,6 +27,8 @@ import org.springframework.web.multipart.MultipartFile;
 @Api(tags = "通用接口")
 @Slf4j
 public class CommonController {
+	@Autowired
+	private AliOssUtil aliOssUtil;
 
 	/**
 	 * 文件上传
@@ -32,6 +40,20 @@ public class CommonController {
 	@ApiOperation("文件上传")
 	public Result<String> upload(MultipartFile file) {    // MultipartFile 是 SpringMVC 提供的一个接口，用于封装上传的文件信息。 参数名必须与表单中上传文件的 name 属性值一致。
 		log.info("文件上传：{}", file);
-		return null;
+
+		try {
+			// 获取原始文件名
+			String originalFilename = file.getOriginalFilename();
+			// 获取文件后缀
+			String suffix = originalFilename.substring(originalFilename.lastIndexOf("."));
+			// 生成新文件名
+			String newFileName = UUID.randomUUID().toString() + suffix;
+			// 上传文件到OSS
+			String filepath = aliOssUtil.upload(file.getBytes(), newFileName);
+			return Result.success(filepath);
+		} catch (IOException e) {
+			log.error("文件上传失败：{}", e);
+		}
+		return Result.error(MessageConstant.UPLOAD_FAILED);
 	}
 }
