50b037fa6a6638e7dafeeac3366af9329240a749
daykalif <1038200595@qq.com>
2025-02-12 23:13:43 +0800

    feat: day06-20-导入商品浏览功能代码_代码导入和功能测试 && day04中缺少的代码

--- sky-server/src/main/java/com/sky/mapper/SetmealDishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealDishMapper.java
@@ -1,5 +1,6 @@
 package com.sky.mapper;
 
+import com.sky.entity.SetmealDish;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -14,4 +15,12 @@ public interface SetmealDishMapper {
 	 */
 	// SELECT setmeal_id FROM setmeal_dish WHERE dish_id IN (1,2,3,4,5)
 	List<Long> getSetmealIdsByDishIds(List<Long> dishIds);
+
+
+	/**
+	 * 批量保存套餐和菜品的关联关系
+	 *
+	 * @param setmealDishes
+	 */
+	void insertBatch(List<SetmealDish> setmealDishes);
 }
