17b75eaa8d1bcd0d68bc457e339211c31f8d380d
daykalif <1038200595@qq.com>
2025-02-13 18:05:37 +0800

    feat: day07-04-缓存菜品_代码开发_清理缓存数据

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -10,9 +10,11 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Set;
 
 /**
  * 菜品管理
@@ -25,11 +27,21 @@ public class DishController {
 	@Autowired
 	public DishService dishService;
 
+	@Autowired
+	private RedisTemplate redisTemplate;
+
+
 	@PostMapping
 	@ApiOperation("新增菜品")
 	public Result save(@RequestBody DishDTO dishDTO) {
 		log.info("新增菜品:{}", dishDTO);
 		dishService.saveWithFlavor(dishDTO);
+
+		// 清理缓存数据
+		String key = "dish_" + dishDTO.getCategoryId();
+		cleanCache(key);
+
+
 		return null;
 	}
 
@@ -57,6 +69,10 @@ public class DishController {
 	public Result delete(@RequestParam List<Long> ids) {    // @RequestParam：表示将请求参数绑定到方法参数上，参数名必须一致
 		log.info("菜品批量删除:{}", ids);
 		dishService.deleteBatch(ids);
+
+		// 将所有的菜品缓存数据清理掉，所有以dish_开头的key
+		cleanCache("dish_*");
+
 		return Result.success();
 	}
 
@@ -85,8 +101,22 @@ public class DishController {
 	@ApiOperation("修改菜品")
 	public Result update(@RequestBody DishDTO dishDTO) {
 		log.info("修改菜品:{}", dishDTO);
-		dishService.updateWithFlavor(dishDTO);	// 可以修改菜品，也可以修改菜品口味
+		dishService.updateWithFlavor(dishDTO);    // 可以修改菜品，也可以修改菜品口味
+
+		// 将所有的菜品缓存数据清理掉，所有以dish_开头的key
+		cleanCache("dish_*");
+
 		return Result.success();
 	}
 
+
+	/**
+	 * 清理缓存数据
+	 *
+	 * @param pattern
+	 */
+	private void cleanCache(String pattern) {
+		Set keys = redisTemplate.keys(pattern);
+		redisTemplate.delete(keys);
+	}
 }
