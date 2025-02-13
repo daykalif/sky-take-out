3de92619709281ce013e1daf6d7628b8c0f23047
daykalif <1038200595@qq.com>
2025-02-13 22:14:35 +0800

    feat: day07-08-缓存套餐_Spring Cache_保存数据到redis

--- 课程笔记/day07/2.缓存套餐/springcache-demo/src/main/java/com/itheima/controller/UserController.java
+++ 课程笔记/day07/2.缓存套餐/springcache-demo/src/main/java/com/itheima/controller/UserController.java
@@ -11,29 +11,33 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 public class UserController {
 
-    @Autowired
-    private UserMapper userMapper;
+	@Autowired
+	private UserMapper userMapper;
 
-    @PostMapping
-    public User save(@RequestBody User user){
-        userMapper.insert(user);
-        return user;
-    }
+	@PostMapping
+	//@CachePut(cacheNames = "userCache", key = "#user.id")   // 如果使用Spring Cache缓存数据，key的生成：userCache::xxx
+	//@CachePut(cacheNames = "userCache", key = "#result.id") // 对象导航
+	//@CachePut(cacheNames = "userCache", key = "#p0.id") // 获取第一个参数的id
+	@CachePut(cacheNames = "userCache", key = "#a0.id") // 获取第一个参数的id
+	public User save(@RequestBody User user) {
+		userMapper.insert(user);
+		return user;
+	}
 
-    @DeleteMapping
-    public void deleteById(Long id){
-        userMapper.deleteById(id);
-    }
+	@DeleteMapping
+	public void deleteById(Long id) {
+		userMapper.deleteById(id);
+	}
 
 	@DeleteMapping("/delAll")
-    public void deleteAll(){
-        userMapper.deleteAll();
-    }
+	public void deleteAll() {
+		userMapper.deleteAll();
+	}
 
-    @GetMapping
-    public User getById(Long id){
-        User user = userMapper.getById(id);
-        return user;
-    }
+	@GetMapping
+	public User getById(Long id) {
+		User user = userMapper.getById(id);
+		return user;
+	}
 
 }
