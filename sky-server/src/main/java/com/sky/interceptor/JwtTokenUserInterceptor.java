d3c0de33a832dd20df22f0ecf04c7a73fb55d54d
daykalif <1038200595@qq.com>
2025-02-14 22:13:56 +0800

    feat: day07-17-添加购物车

--- sky-server/src/main/java/com/sky/interceptor/JwtTokenUserInterceptor.java
+++ sky-server/src/main/java/com/sky/interceptor/JwtTokenUserInterceptor.java
@@ -48,10 +48,11 @@ public class JwtTokenUserInterceptor implements HandlerInterceptor {
 		//2、校验令牌
 		try {
 			log.info("jwt校验:{}", token);
+
 			Claims claims = JwtUtil.parseJWT(jwtProperties.getUserSecretKey(), token);
 			Long userId = Long.valueOf(claims.get(JwtClaimsConstant.USER_ID).toString());
+			log.info("当前用户id################>:{}", userId);
 
-			log.info("当前用户id：", userId);
 			// 将当前登录用户的id存入到当前线程的LocalThread中
 			BaseContext.setCurrentId(userId);
 
