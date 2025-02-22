16dcd0bca27daaafa704cd928d1de280c15d51bc
daykalif <1038200595@qq.com>
2025-02-22 18:04:01 +0800

    feat: day12-04-工作台-代码导入&功能测试

--- sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
@@ -8,6 +8,7 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * 套餐Mapper
@@ -70,4 +71,13 @@ public interface SetmealMapper {
 	 */
 	@Select("select * from setmeal where id = #{id}")
 	Setmeal getById(Long id);
+
+
+	/**
+	 * 根据条件统计套餐数量
+	 *
+	 * @param map
+	 * @return
+	 */
+	Integer countByMap(Map map);
 }
